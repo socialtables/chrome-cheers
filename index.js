00ff35e4818a6feb28a07fa78e0fb6abf374b6f1
Ricky Vetter <ricky@socialtables.com>
2015-03-09 22:25:51 -0400

    start example project and parse out token

---
+++ index.js
@@ -0,0 +1,42 @@
+var urlParse = require("url");
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.url) {
+    updateToken(tab.url, tabId);
+  }
+});
+
+chrome.tabs.onCreated.addListener(handleTab);
+
+function handleTab(tab) { 
+  if (tab.url) {
+    updateToken(tab.url, tab.id); 
+  }
+}
+
+function updateToken(url, tabId) {
+  console.log("ricky");
+  var urlObject = urlParse.parse(url);
+  console.log(urlObject);
+  var isTinyPulse = urlObject.host.indexOf("tinypulse.com") !== -1;
+  var hasParams = urlObject.query;
+  var hasResponseToken = hasParams && urlObject.query.indexOf("response_token") !== -1;
+  if(isTinyPulse &&  hasResponseToken) {
+    var splitQueries = urlObject.query.split("&");
+    var params = splitQueries.reduce(function(params, query) {
+      var splitQuery = query.split("=");
+      params[splitQuery[0]] = splitQuery[1];
+      return params;
+    }, {});
+    console.log(params.response_token);
+  }
+
+  // var funding = findCompanyByHostAndPath(getHostAndPath(url));
+  // if (funding !== undefined) {
+  //   var formatted = formatDollars(funding[0]);
+  //   chrome.browserAction.setBadgeText({ text: formatted[0], tabId: tabId });
+  //   chrome.browserAction.setBadgeBackgroundColor({ color: formatted[1], tabId: tabId });
+  // } else {
+  //   chrome.browserAction.disable(tabId);
+  // }
+}
