c1be0a7e73f21d63be2e0d61ee180beab94f5518
Ricky Vetter <ricky@socialtables.com>
2015-03-29 10:37:35 -0400

    add typeahead and better notifications. start using cheers.rocks

--- popup.js
+++ popup.js
@@ -1,13 +1,17 @@
 var cheers = require("tinypulse").Cheers;
+var xhr = require("xhr");
+var Typeahead = require("typeahead");
 
 var token = null;
+var tokenText = "";
+var apiBase = "http://www.cheers.rocks";
 
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
 function submitCheers(event) {
-	console.log("Cheers!", arguments);
+	renderStatus("Sending ... ");
 	var email = document.getElementById("recipient").value.toLowerCase().trim();
 	var message = document.getElementById("message").value;
 	var isAnonymous = document.getElementById("anonymous").checked;
@@ -22,22 +26,57 @@ function submitCheers(event) {
 		}
 		else {
 			renderStatus("Cheers sent!");
-			window.setTimeout(renderStatus, 1000, token);
+			window.setTimeout(renderStatus, 1000, tokenText);
 		}
 	});
 	
 }
 
+var setDisabledAllFields = function(isDisabled) {
+	var fields = [
+		document.getElementById("recipient"),
+		document.getElementById("message"),
+		document.getElementById("anonymous"),
+		document.getElementById("submit-cheers")
+	];
+	fields.forEach(function(field) {
+		field.disabled = isDisabled
+	})
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+  renderStatus("Fetching token and loading users...");
+
   chrome.storage.sync.get('response_token', function(response) {
     token = response.response_token;
     if(!token) {
     	renderStatus("No token found. Please visit TinyPulse from your email. We'll grab it when we can.");
+    	setDisabledAllFields(true);
     }
-    renderStatus(token);
+    else {
+    	// verify token
+    	xhr({
+    		uri: apiBase + "/token/new",
+    		method: "POST",
+    		json: {token: token}
+    	}, function(err, resp, body) {
+    		if(err || !body.verified) {
+    			renderStatus("Token is not valid. Please visit TinyPulse from your email. We'll grab a new one when we can.");
+    			setDisabledAllFields(true);
+    			throw new Error("Token is not valid.", err);
+    		}
+    		tokenText = "Sending as: " + body.email + " (" + token + ")"
+    		renderStatus(tokenText);
 
-    var button = document.getElementById("submit-cheers");
-    console.log(button);
-    button.addEventListener("click", submitCheers);
+    		var recipient = document.getElementById("recipient");
+    		var ta = Typeahead(recipient, {
+    		    source: body.possibleRecipients
+    		});
+
+    		var button = document.getElementById("submit-cheers");
+    		button.addEventListener("click", submitCheers);
+    	});
+    }
+    
   });
 });
